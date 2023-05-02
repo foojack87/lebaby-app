5fc93fd0d3919cda975e84ab5bfac02a490f74f1
unknown <foojack@gmail.com>
2023-05-02 13:58:50 +0800

    fixed bug with favicon not showing

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/image/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
